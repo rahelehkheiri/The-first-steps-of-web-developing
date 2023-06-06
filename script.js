72a3b0af252d868c25f66431d6f65d47b74cfe2b
Raheleh.kheiri <r.kheiri9693@gmail.com>
2023-06-06 18:09:08 +0430

    Modal for login was added but it is not completed

---
+++ script.js
@@ -0,0 +1,11 @@
+let loginModal = document.getElementById(loginModal)
+let loginExit1 = document.getElementById(login-exit1)
+let loginExit = document.getElementById(login-exit)
+
+loginExit.addEventListener('click', ()=>{
+    loginModal.display('none')
+})
+
+loginExit1.addEventListener('click', ()=>{
+    loginModal.display('none')
+})
